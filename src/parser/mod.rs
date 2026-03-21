a8ed6b664390c5bb26ea288626c4760dc0714c7e
Ranga Raja R acko <r34rangaraja@gmail.com>
2026-03-21 15:14:43 +0530

    wip(parser): added basic model

---
+++ src/parser/mod.rs
@@ -0,0 +1,4 @@
+#[allow(unused_imports)]
+pub mod model;
+
+pub use model::Schema;
---
+++ src/parser/model.rs
@@ -0,0 +1,15 @@
+#![allow(dead_code)]
+use std::collections::HashMap;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type")]
+pub enum Schema {
+    #[serde(rename = "int")]
+    Int { min: Option<i32>, max: Option<i32> },
+    #[serde(rename = "float")]
+    Float { min: Option<f32>, max: Option<f32> },
+    #[serde(rename = "object")]
+    Object { properties: HashMap<String, Schema> },
+}
